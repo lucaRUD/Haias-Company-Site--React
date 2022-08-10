b10ba4e44483ebe3ea67c4fe265bdb3b5fd1970c
lucaRUD <rudolf.stancu01@e-uvt.ro>
2022-08-10 17:28:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
+
+import Tinting from './components/pages/Tinting';
+import Wrapping from './components/pages/Wrapping';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Branding from './components/pages/Branding';
+import Buycar from './components/pages/Buycar';
+import Footer from './components/Footer';
+import ScrollToTop from './components/scrollToTop';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <ScrollToTop />
+      <Navbar />
+
+      <Switch>
+
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Buycar} />
+        <Route path='/tinting' exact component={Tinting} />
+        <Route path='/contact' exact component={Contact} />
+        <Route path='/wrapping' exact component={Wrapping} />
+        <Route path='/branding' exact component={Branding} />
+        <Route path='/buycar' exact component={Buycar} />
+
+      </Switch>
+      <Footer />
+    </Router >
   );
 }
 

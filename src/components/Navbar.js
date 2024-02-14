0f63a0e18e01744fee5b2ca89980228a4553ed1b
Luca Stancu <rudolf.stancu01@e-uvt.ro>
2024-02-15 00:19:48 +0200

    Update 2024 V-Day

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,13 +47,13 @@ function Navbar() {
          onMouseEnter={onMouseEnter}
          onMouseLeave={onMouseLeave}
        >
-              <Link to='/Haias-Company-Site--React/services' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
               Services <i className='fas fa-caret-down' />
               </Link>
               {dropdown && <Dropdown/>}
             </li>
             <li className='nav-item'>
-              <Link to='/Haias-Company-Site--React/contact' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
               Contact
               </Link>
             </li>
